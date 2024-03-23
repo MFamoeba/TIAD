95675894c006b3ef568b97ea0ad4d3d647c2c80e
MFamoeba <161942152+MFamoeba@users.noreply.github.com>
2024-03-23 10:32:13 +0100

    uneccesary method removed

--- Zad1/algo/tools.py
+++ Zad1/algo/tools.py
@@ -5,19 +5,6 @@ def swarm_generator(pop_size=30, lower_bound=-5, upper_bound=5, dimension=2):
     return np.random.uniform(low=lower_bound, high=upper_bound, size=(pop_size, dimension))
 
 
-def find_new_best_positions(positions_a, positions_b, fitness_a, fitness_b):
-    new_best_positions = []
-    new_best_fitness = []
-    for i in range(len(positions_a)):
-        if fitness_a[i] < fitness_b[i]:
-            new_best_positions.append(positions_a[i])
-            new_best_fitness.append(fitness_a[i])
-        else:
-            new_best_positions.append(positions_b[i])
-            new_best_fitness.append(fitness_b[i])
-    return np.array(new_best_positions), np.array(new_best_fitness)
-
-
 def find_new_best_positions(positions_a, positions_b, objective_func):
     fitness_a = [objective_func(particle) for particle in positions_a]
     fitness_b = [objective_func(particle) for particle in positions_b]
